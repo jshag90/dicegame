eaea2603577fb9e0ef6dd19609f16f658183e71f
seonhakji <jshag1990@gmail.com>
2024-12-02 22:54:35 +0900

    [Feat] war export gradle 설정

--- src/main/java/com/dodam/dicegame/dicegame/DiceGameApplication.java
+++ src/main/java/com/dodam/dicegame/dicegame/DiceGameApplication.java
@@ -2,9 +2,10 @@ package com.dodam.dicegame.dicegame;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DiceGameApplication {
+public class DiceGameApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DiceGameApplication.class, args);
