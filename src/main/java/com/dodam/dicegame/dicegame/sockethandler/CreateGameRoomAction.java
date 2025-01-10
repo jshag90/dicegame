b6954e705f5f010460b13b7f89bdb61e9bc84a15
seonhakji <jshag1990@gmail.com>
2025-01-10 23:51:47 +0900

    [Feat] 방 개설 action 추가

---
+++ src/main/java/com/dodam/dicegame/dicegame/sockethandler/CreateGameRoomAction.java
@@ -0,0 +1,39 @@
+package com.dodam.dicegame.dicegame.sockethandler;
+
+import com.dodam.dicegame.dicegame.repository.RoomRepository;
+import com.dodam.dicegame.dicegame.service.WebSocketRoomService;
+import com.dodam.dicegame.dicegame.vo.ResponseSocketPayloadVO;
+import com.dodam.dicegame.dicegame.vo.SocketPayloadVO;
+import com.google.gson.Gson;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.socket.WebSocketSession;
+
+@RequiredArgsConstructor
+@Component
+@Slf4j
+public class CreateGameRoomAction implements BroadcastByActionType {
+
+    private final WebSocketRoomService webSocketRoomService;
+
+    private final Gson gson;
+
+    @Override
+    public boolean isAction(String action) {
+        return "createGameRoom".equals(action);
+    }
+
+    @Override
+    public void broadcastToClient(WebSocketSession session, SocketPayloadVO socketPayloadVO) {
+        log.info("TTTTTTTTTTTTTTTTTTTTTTT{}", session.getId());
+        webSocketRoomService.addSessionToRoom(socketPayloadVO.getRoomId(), session.getId(), socketPayloadVO.getUuid(), session);
+        ResponseSocketPayloadVO responseSocketPayloadVO = ResponseSocketPayloadVO.builder()
+                .action(socketPayloadVO.getAction())
+                .message(socketPayloadVO.getRoomId() + "번방을 개설하였습니다.")
+                .build();
+        broadcastToRoom(webSocketRoomService, socketPayloadVO.getRoomId(), null, gson.toJson(responseSocketPayloadVO));
+    }
+
+}
